353ada4fbe2acebe6ba2d2045c8510184d0a899a
David Bujosa <davidbujosa@gmail.com>
2022-03-28 00:01:13 +0200

    feat: add lastscore and high score logic

--- app/src/main/java/com/bujosa/andromeda/MainActivity.java
+++ app/src/main/java/com/bujosa/andromeda/MainActivity.java
@@ -11,10 +11,11 @@ import java.text.MessageFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView timerTextView, currentTapTextView;
+    private TextView timerTextView, currentTapTextView, highScoreTextView, lastScoreTextView;
     private final long initialCountDown = 60000;
     private final int timerInterval = 1000;
     private int reamingTime = 60;
+    private int lastScore = 0, highScore = 0;
     private boolean status = true;
     private int currentTap = 0;
 
@@ -24,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         timerTextView = findViewById(R.id.timerTextView);
-
         currentTapTextView = findViewById(R.id.currentTapTextView);
+        lastScoreTextView = findViewById(R.id.lastScoreTextView);
+        highScoreTextView = findViewById(R.id.highScoreTextView);
 
         currentTapTextView.setText(MessageFormat.format("{0}", currentTap));
 
@@ -40,10 +42,17 @@ public class MainActivity extends AppCompatActivity {
             public void onFinish() {
                 Toast.makeText(MainActivity.this, "CountDown Finished", Toast.LENGTH_LONG).show();
                 status = true;
+                if(currentTap >= highScore){
+                    highScore = currentTap;
+                }
+                lastScore = currentTap;
                 currentTap = 0;
                 reamingTime = 60;
+                highScoreTextView.setText(MessageFormat.format("{0}", highScore));
+                lastScoreTextView.setText(MessageFormat.format("{0}", lastScore));
                 currentTapTextView.setText(MessageFormat.format("{0}", currentTap));
                 timerTextView.setText(MessageFormat.format("{0}", reamingTime));
+
             }
         };
 
